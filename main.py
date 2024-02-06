ad0139099a7714b3b2b629a73d1fb098a53e6359
chisoftltd <chisoftltd@gmail.com>
2024-02-06 15:21:18 +0100

    Advanced Decorator

--- main.py
+++ main.py
@@ -3,20 +3,41 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return ('<h1 style="text-align: center">Hello, World!</h1>'
-            '<p style="text-align: center">This is a paragraph</p>'
-            '<img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTkxYjhlb21lMHliNTZrYXk0ZjJwYWV6ZGkyMTR2Mm9yaHFld3FqMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/C8oGI85zRUqWhcwnVR/giphy.gif" style="text-align: center">')
+# @app.route('/')
+# def hello_world():
+#     return ('<h1 style="text-align: center">Hello, World!</h1>'
+#             '<p style="text-align: center">This is a paragraph</p>'
+#             '<img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTkxYjhlb21lMHliNTZrYXk0ZjJwYWV6ZGkyMTR2Mm9yaHFld3FqMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/C8oGI85zRUqWhcwnVR/giphy.gif" style="text-align: center">')
+#
+# @app.route("/bye")
+# def say_bye():
+#     return "Bye"
+#
+# @app.route("/username/<name>/<int:number>")
+# def greet(name, number):
+#     return f"Hello there {name}, you are {number} years old!"
+#
+# if __name__ == "__main__":
+#     #Reload and auto debugger
+#     app.run(debug=True)
 
-@app.route("/bye")
-def say_bye():
-    return "Bye"
+## Advanced Python Decorator Function
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.is_logged_in = False
 
-@app.route("/username/<name>/<int:number>")
-def greet(name, number):
-    return f"Hello there {name}, you are {number} years old!"
+def is_authenticated_decorator(function):
+    def wrapper(*args, **kwargs):
+        if args[0].is_logged_in == True:
+            function(args[0])
+    return wrapper
 
-if __name__ == "__main__":
-    #Reload and auto debugger
-    app.run(debug=True)
+@is_authenticated_decorator
+def create_blog_post(user):
+    print(f"This is {user.name}'s new blog post.")
+
+
+new_user = User("Benjamin")
+new_user.is_logged_in = True
+create_blog_post(new_user)
