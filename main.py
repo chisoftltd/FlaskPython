440feb4fc620a9f73cf879239ae7e3387d3d692a
chisoftltd <chisoftltd@gmail.com>
2024-02-06 14:53:58 +0100

    Flask html

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return ('<h1 style="text-align: center">Hello, World!</h1>'
+            '<p style="text-align: center">This is a paragraph</p>'
+            '<img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTkxYjhlb21lMHliNTZrYXk0ZjJwYWV6ZGkyMTR2Mm9yaHFld3FqMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/C8oGI85zRUqWhcwnVR/giphy.gif" style="text-align: center">')
 
 @app.route("/bye")
 def say_bye():
@@ -16,4 +18,5 @@ def greet(name, number):
     return f"Hello there {name}, you are {number} years old!"
 
 if __name__ == "__main__":
+    #Reload and auto debugger
     app.run(debug=True)
